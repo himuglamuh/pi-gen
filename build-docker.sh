c4ddd49f26500a72cc71d39c3a0929a71b02d08c
F3r41OutL4w <himuglamuh@proton.me>
2025-09-08 11:56:05 -0700

    Fix base image logic for amd64 host and first draft beaconbox stage

--- build-docker.sh
+++ build-docker.sh
@@ -83,14 +83,15 @@ fi
 BUILD_OPTS="$(echo "${BUILD_OPTS:-}" | sed -E 's@\-c\s?([^ ]+)@-c /config@')"
 
 # Check the arch of the machine we're running on. If it's 64-bit, use a 32-bit base image instead
-case "$(uname -m)" in
-  x86_64|aarch64)
-    BASE_IMAGE=i386/debian:trixie
-    ;;
-  *)
-    BASE_IMAGE=debian:trixie
-    ;;
-esac
+# case "$(uname -m)" in
+#   x86_64|aarch64)
+#     BASE_IMAGE=i386/debian:trixie
+#     ;;
+#   *)
+#     BASE_IMAGE=debian:trixie
+#     ;;
+# esac
+BASE_IMAGE=debian:bookworm
 ${DOCKER} build --build-arg BASE_IMAGE=${BASE_IMAGE} -t pi-gen "${DIR}"
 
 if [ "${CONTAINER_EXISTS}" != "" ]; then
