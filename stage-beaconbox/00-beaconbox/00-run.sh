16d4409b123039fec7b2b04cf5029322158ae263
F3r41OutL4w <himuglamuh@proton.me>
2025-09-08 13:02:40 -0700

    cont wip

--- stage-beaconbox/00-beaconbox/00-run.sh
+++ stage-beaconbox/00-beaconbox/00-run.sh
@@ -1,11 +1,9 @@
-#!/bin/bash
-set -e
+OVERLAY_DIR="$(dirname "$0")/../overlay"
 
-install -m 0755 -o root -g root -d "${ROOTFS_DIR}/etc/beaconbox"
-cp -r /stage2/99-beaconbox/etc/* "${ROOTFS_DIR}/etc/"
-
-install -m 0755 -o root -g root -d "${ROOTFS_DIR}/usr/local/bin"
-cp -r /stage2/99-beaconbox/usr/local/bin/* "${ROOTFS_DIR}/usr/local/bin/"
-
-install -m 0755 -o root -g root -d "${ROOTFS_DIR}/srv/beaconbox"
-cp -r /stage2/99-beaconbox/srv/* "${ROOTFS_DIR}/srv/"
+if [ -d "${OVERLAY_DIR}" ]; then
+    echo "Applying overlay from ${OVERLAY_DIR} to ${ROOTFS_DIR}..."
+    cp -ar "${OVERLAY_DIR}/." "${ROOTFS_DIR}/"
+else
+    echo "Overlay directory not found: ${OVERLAY_DIR}"
+    exit 1
+fi
