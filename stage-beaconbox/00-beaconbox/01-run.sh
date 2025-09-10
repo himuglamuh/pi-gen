5238411991d7b8d28cc97833e014907a854a01ca
F3r41OutL4w <himuglamuh@proton.me>
2025-09-10 15:49:33 -0700

    wip - correct sudoers

--- stage-beaconbox/00-beaconbox/01-run.sh
+++ stage-beaconbox/00-beaconbox/01-run.sh
@@ -7,6 +7,13 @@ mkdir -p "${ROOTFS_DIR}"
 echo "👉 Copying common overlay files..."
 rsync -a --chown=1000:1000 "/pi-gen/common-overlay/" "${ROOTFS_DIR}/"
 
+echo "👉 Setting permissions on /etc/sudoers.d and system files..."
+chown root:root "${ROOTFS_DIR}/etc/sudoers.d"
+chmod 755 "${ROOTFS_DIR}/etc/sudoers.d"
+find "${ROOTFS_DIR}/etc/sudoers.d" -type f -exec chown root:root {} \;
+find "${ROOTFS_DIR}/etc/sudoers.d" -type f -exec chmod 440 {} \;
+find "${ROOTFS_DIR}/etc/systemd/system" -name '*.service' -exec chown root:root {} \;
+
 echo "👉 Setting beaconbox net and web services..."
 chroot "${ROOTFS_DIR}" systemctl enable beaconbox-net.service
 chroot "${ROOTFS_DIR}" systemctl enable beaconbox-web.service
