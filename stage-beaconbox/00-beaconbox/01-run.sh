ab532c375c74f01a0f1b701209a75cd7a3e622bb
F3r41OutL4w <himuglamuh@proton.me>
2025-09-15 08:42:03 -0700

    tweak yaml handling, captive portal handling

--- stage-beaconbox/00-beaconbox/01-run.sh
+++ stage-beaconbox/00-beaconbox/01-run.sh
@@ -14,6 +14,10 @@ find "${ROOTFS_DIR}/etc/sudoers.d" -type f -exec chown root:root {} \;
 find "${ROOTFS_DIR}/etc/sudoers.d" -type f -exec chmod 440 {} \;
 find "${ROOTFS_DIR}/etc/systemd/system" -name '*.service' -exec chown root:root {} \;
 
+echo "👉 Updating initramfs..."
+
+chroot "${ROOTFS_DIR}" update-initramfs -u
+
 echo "👉 Setting beaconbox net and web services..."
 chroot "${ROOTFS_DIR}" systemctl disable dnsmasq.service || true
 chroot "${ROOTFS_DIR}" systemctl enable beaconbox-net.service
