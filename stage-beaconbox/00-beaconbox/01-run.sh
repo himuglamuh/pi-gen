0d2b23219278980002482711b1cc94a12f2d26a1
F3r41OutL4w <himuglamuh@proton.me>
2025-09-16 11:13:53 -0700

    change sudoer logic

--- stage-beaconbox/00-beaconbox/01-run.sh
+++ stage-beaconbox/00-beaconbox/01-run.sh
@@ -7,12 +7,11 @@ mkdir -p "${ROOTFS_DIR}"
 echo "👉 Copying common overlay files..."
 rsync -a --chown=1000:1000 "/pi-gen/common-overlay/" "${ROOTFS_DIR}/"
 
-echo "👉 Setting permissions on /etc/sudoers.d and system files..."
-chown root:root "${ROOTFS_DIR}/etc/sudoers.d"
-chmod 755 "${ROOTFS_DIR}/etc/sudoers.d"
-find "${ROOTFS_DIR}/etc/sudoers.d" -type f -exec chown root:root {} \;
-find "${ROOTFS_DIR}/etc/sudoers.d" -type f -exec chmod 440 {} \;
-find "${ROOTFS_DIR}/etc/systemd/system" -name '*.service' -exec chown root:root {} \;
+echo "👉 Setting sudoer permissions..."
+source "${ROOTFS_DIR}/config"
+USERNAME="${FIRST_USER_NAME}"
+echo "$USERNAME ALL=(ALL) NOPASSWD: ALL" > "${ROOTFS_DIR}/etc/sudoers.d/010_beaconbox"
+chmod 0440 "${ROOTFS_DIR}/etc/sudoers.d/010_beaconbox"
 
 echo "👉 Disabling unnecessary services to speed up boot..."
 
@@ -46,6 +45,8 @@ chroot "${ROOTFS_DIR}" systemctl disable dnsmasq.service || true
 chroot "${ROOTFS_DIR}" systemctl enable beaconbox-net.service
 chroot "${ROOTFS_DIR}" systemctl enable beaconbox-web.service
 chroot "${ROOTFS_DIR}" systemctl enable beaconbox-dns.service
+chroot "${ROOTFS_DIR}" systemctl enable beaconbox-usb-mount@.service
+chroot "${ROOTFS_DIR}" systemctl enable beaconbox-usb-cleanup.service
 chroot "${ROOTFS_DIR}" systemctl enable nftables.service
 
 echo "👉 Touching beaconbox installed file..."
